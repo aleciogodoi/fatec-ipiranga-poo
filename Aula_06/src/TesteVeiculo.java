bd55f39d9d77fb372a2afd9e8ee55729f7c953d0
Alécio <alecio.godoi@ymail.com>
2020-09-25 22:07:18 -0300

    Classe veiculo

---
+++ Aula_06/src/TesteVeiculo.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class TesteVeiculo {
+
+	public static void main(String[] args) {
+		ArrayList<Veiculo> veiculos = new ArrayList<Veiculo>();
+		veiculos.add(new Veiculo("XPO-0987",2010));
+		veiculos.add(new Veiculo("ABC-1234",2012));
+		veiculos.add(new Veiculo("KYX-0987",2014));
+		
+		for(Veiculo veiculo: veiculos) {
+			veiculo.exibirDados();
+		}
+	}
+
+}
---
+++ Aula_06/src/Veiculo.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+
+public class Veiculo {
+	private String placa;
+	private int ano;
+	
+	public Veiculo() {
+	}
+	
+	public Veiculo(String placa, int ano) {
+		super();
+		this.placa = placa;
+		this.ano = ano;
+	}
+	
+	@Override
+	public String toString() {
+		return "Veiculo [placa=" + placa + ", ano=" + ano + "]";
+	}
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+	public void exibirDados() {
+		JOptionPane.showMessageDialog(null, this.toString());
+	}
+}
