acd75b840f28e4ad87d4db077d4ebc2cae92f0d2
Alécio <alecio.godoi@ymail.com>
2020-10-02 21:16:19 -0300

    Aula 07 - Lab - Data e Pessoa

---
+++ Aula_07_Lab/src/Pessoa.java
@@ -0,0 +1,30 @@
+
+public class Pessoa {
+	private String nome;
+	private Data nascimento;
+	
+	public Pessoa() {
+	}
+	public Pessoa(String nome, Data nascimento) {
+		this.nome = nome;
+		this.nascimento = nascimento;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Data getNascimento() {
+		return nascimento;
+	}
+	public void setNascimento(Data nascimento) {
+		this.nascimento = nascimento;
+	}
+	@Override
+	public String toString() {
+		return "Pessoa [nome=" + nome + ", nascimento=" + nascimento + "]";
+	}
+
+	
+}
